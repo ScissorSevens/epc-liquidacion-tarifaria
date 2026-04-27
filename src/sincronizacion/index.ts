455b3b8c55dc0399300574c45685a64398f5f2ca
Felipe Bernal <felipeb.pachon@gmail.com>
2026-04-27 16:01:33 -0500

    feat(sincronizacion): implementar InMemoryColaSincronizacion con Repository Pattern (GREEN)

--- src/sincronizacion/index.ts
+++ src/sincronizacion/index.ts
@@ -1,2 +1,4 @@
 export { agregarItemACola } from './cola';
+export { InMemoryColaSincronizacion } from './cola-repository';
+export type { ColaSincronizacion } from './cola-repository';
 export type { ItemCola, AgregarItemInput, TipoItem, EstadoItem } from './types';
