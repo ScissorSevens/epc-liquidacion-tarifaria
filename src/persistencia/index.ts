cc4b1784a42f103ce50ea76b37d781642619d625
Felipe Bernal <felipeb.pachon@gmail.com>
2026-05-04 16:33:12 -0500

    feat(persistencia): exporta crearBootstrapLecturaSqlite desde index publico

--- src/persistencia/index.ts
+++ src/persistencia/index.ts
@@ -1,2 +1,9 @@
 export { LecturaRepositoryMemoria } from './lectura-repository-memoria';
 export type { LecturaRepository, FiltrosLectura, ResultadoPaginado } from './lectura-repository';
+export { crearLecturaRepositorySqlite } from './sqlite/lectura-repository-sqlite';
+export type { LecturaRepositorySqlite } from './sqlite/lectura-repository-sqlite';
+export { crearBootstrapLecturaSqlite } from './sqlite/bootstrap-lectura';
+export type {
+  BootstrapLecturaSqlite,
+  BootstrapLecturaSqliteOpciones,
+} from './sqlite/bootstrap-lectura';
