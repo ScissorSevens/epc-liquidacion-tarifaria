691ab011151d0f8edf14261461ba08f84f00cf16
Felipe Bernal <felipeb.pachon@gmail.com>
2026-07-09 15:07:44 -0500

    feat(domain): export crearPrestador from prestadores barrel

--- mobile/dominio/prestadores/index.ts
+++ mobile/dominio/prestadores/index.ts
@@ -1,3 +1,3 @@
 export type { Prestador, PrestadorBorrador, CrearPrestadorInput, ActualizarPrestadorInput, EstadoPrestador, SegmentoPrestador, FiltrosListarPrestador, PrestadorRepository } from './types';
 export { MENSAJES_ERROR_PRESTADOR } from './types';
-export { cedulaRepresentanteLegalValida } from './validador-prestador';
+export { cedulaRepresentanteLegalValida, crearPrestador } from './validador-prestador';
